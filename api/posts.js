a4445287b37868b1097cf86cc0cb5a254a549048
Krzychu159 <krzysztof.rak159@gmail.com>
2025-05-16 19:27:19 +0200

    Update

--- api/posts.js
+++ api/posts.js
@@ -609,7 +609,19 @@ let posts = [
   },
 ];
 
-async function handler(req, res) {
+export default function handler(req, res) {
+  // CORS HEADERS
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end(); // Preflight
+  }
+
   const { method, url } = req;
   const match = url.match(/\/api\/posts\/?(\d+)?/);
   const id = match?.[1] ? parseInt(match[1]) : null;
@@ -651,5 +663,3 @@ async function handler(req, res) {
 
   return res.status(405).json({ message: "Method not allowed" });
 }
-
-export default cors(handler);
