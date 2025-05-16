29d4c78c7b5beb609d65080c2f0e00f1e23b59de
Krzychu159 <krzysztof.rak159@gmail.com>
2025-05-16 19:37:46 +0200

    Update

--- api/posts.js
+++ api/posts.js
@@ -616,7 +616,7 @@ export default function handler(req, res) {
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, OPTIONS"
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   if (req.method === "OPTIONS") {
     return res.status(200).end(); // Preflight
